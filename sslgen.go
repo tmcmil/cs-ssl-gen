44ec9ce14dbadae1b916471935d228b64f0bb162
Mike Hodges <rmikehodges@gmail.com>
2018-02-06 03:40:26 -0500

    Removed stdout piping to keep the app quieter

--- sslgen.go
+++ sslgen.go
@@ -4,6 +4,7 @@ package main
 //https://cybersyndicates.com/2016/12/egressing-bluecoat-with-cobaltstike-letsencrypt/
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,29 +12,23 @@ import (
 )
 
 func letsEncrypt(domain string) string {
+	fmt.Println("Installing some dependencies..")
 	cmd := exec.Command("apt-get", "-y", "install", "openssl", "git", "apache2")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem installing openssl, git, apache2: %s", err)
 	}
 	cmd = exec.Command("service", "apache2", "start")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem starting apache2: %s", err)
 	}
 	cmd = exec.Command("apt-get", "update")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem updating via apt-get: %s", err)
 	}
+	fmt.Println("Grabbing LetsEncrypt..")
 	if _, err := os.Stat("/opt/letsencrypt"); err != nil {
 		if os.IsNotExist(err) {
 			cmd = exec.Command("git", "clone", "https://github.com/certbot/certbot", "/opt/letsencrypt")
-			cmd.Stderr = os.Stderr
-			cmd.Stdout = os.Stdout
 			if err := cmd.Run(); err != nil {
 				log.Fatalf("Problem git cloning certbot: %s", err)
 			}
@@ -41,15 +36,12 @@ func letsEncrypt(domain string) string {
 			log.Fatalf("Problem checking if letsencrypt directory exists: %s", err)
 		}
 	}
+	fmt.Println("Installing Certificate...")
 	cmd = exec.Command("/opt/letsencrypt/letsencrypt-auto", "--apache", "-d", domain, "-n", "--register-unsafely-without-email", "--agree-tos")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem installing ssl certificate: %s", err)
 	}
 	cmd = exec.Command("service", "apache2", "stop")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Printf("Problem stopping apache2: %s", err)
 	}
@@ -62,25 +54,21 @@ func genCSKeystore(csdir string, domain string, password string) {
 	if !checkCertDeps() {
 		log.Fatal("Check to make sure keytool and openssl are installed and in PATH")
 	}
+	fmt.Println("Generating PKCS12 Keystore")
 	cmd := exec.Command("openssl", "pkcs12", "-export", "-in", letsencryptDir+"/fullchain.pem", "-inkey",
 		letsencryptDir+"/privkey.pem", "-out", letsencryptDir+"/"+domain+".pkcs", "-name", domain,
 		"-passout", "pass:"+password)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem generating PKCS12 certificate: %s", err)
 	}
+	fmt.Println("Generating Java Keystore")
 	cmd = exec.Command("keytool", "-importkeystore", "-deststorepass", password, "-destkeypass", password,
 		"-destkeystore", letsencryptDir+"/"+domain+".store", "-srckeystore", letsencryptDir+"/"+domain+".pkcs",
 		"-srcstoretype", "PKCS12", "-srcstorepass", password, "-alias", domain)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem creating Java keystore: %s", err)
 	}
 	cmd = exec.Command("cp", letsencryptDir+"/"+domain+".store", csdir)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Problem copying keystore to cobalt strike directory: %s", err)
 	}
@@ -106,6 +94,18 @@ func checkCertDeps() bool {
 }
 
 func main() {
-	genCSKeystore("/root", "swansonmedical.com", "Mike6192")
+	flag.Usage = func() {
+		fmt.Println(`Usage: sslgen -c <Cobalt Strike Directory> -d <domain> -p <ssl password>` + "\n")
+	}
+	csDir := flag.String("c", "", "Cobalt Strike directory where the C2 profile resides")
+	domain := flag.String("d", "", "Domain to issue SSL certificate")
+	password := flag.String("p", "", "Password to use for keystores")
+
+	flag.Parse()
+	if *csDir == "" || *domain == "" || *password == "" {
+		fmt.Println("Missing required parameter(s), use the -h switch for usage")
+		return
+	}
+	genCSKeystore(*csDir, *domain, *password)
 	fmt.Println("Your Cobalt Strike keystore has been created and placed in your Cobalt Strike folder. You will need to place it in the same directory as your C2 profile for it to work.")
 }
